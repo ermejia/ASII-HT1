b8aa44d1e2257f48d50cb8b4aa9d5dc616953ee8
oscarrl98 <oscar_rlp@hotmail.es>
2020-08-06 07:23:50 -0600

    Se ha implementado el metodo llenar

--- src/gt/com/antiguaburger/antiguaburgerweb/controller/Extras.java
+++ src/gt/com/antiguaburger/antiguaburgerweb/controller/Extras.java
@@ -1,8 +1,26 @@
 package gt.com.antiguaburger.antiguaburgerweb.controller;
 
+import gt.com.antiguaburger.antiguaburgerweb.modelo.OrderEntity;
 
-public class Extras {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Extras implements IGetItems{
+	@Override
     public OrderEntity llenar(String id) {
-        return null;
+        List<String> lleno = new ArrayList<>();
+        OrderEntity extra = new OrderEntity();
+        switch (id) {
+            case "0":
+                extra.setExtras("-"); extra.setTotal(0);break;
+            case "1":
+                extra.setExtras("Huevo"); extra.setTotal(1); break;
+            case "2":
+               extra.setExtras("Frijoles"); extra.setTotal(4); break;
+            case "3":
+                extra.setExtras("Plátanos"); extra.setTotal(4); break;
+        }
+        return extra;
     }
 }
