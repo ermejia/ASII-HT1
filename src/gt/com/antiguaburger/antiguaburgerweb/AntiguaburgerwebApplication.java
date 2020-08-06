c70edf85b00792666aec068b47050048020eafd7
ronald <ottoniel.21@outlook.es>
2020-08-06 00:34:51 -0600

    Se añade al proyecto las dependencias necesarias para trabajar con Spring Boot

---
+++ src/gt/com/antiguaburger/antiguaburgerweb/AntiguaburgerwebApplication.java
@@ -0,0 +1,13 @@
+package gt.com.antiguaburger.antiguaburgerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiguaburgerwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiguaburgerwebApplication.class, args);
+	}
+
+}
