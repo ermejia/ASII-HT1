39ea9efab80d4195ee7c3ae7ec0109678c5fdb66
ronald <ottoniel.21@outlook.es>
2020-08-06 19:15:06 -0600

    Se listan los diferentes items de cada menu

--- src/gt/com/antiguaburger/antiguaburgerweb/controller/Menu.java
+++ src/gt/com/antiguaburger/antiguaburgerweb/controller/Menu.java
@@ -1,7 +1,67 @@
 package gt.com.antiguaburger.antiguaburgerweb.controller;
 
-public class Menu {
+import gt.com.antiguaburger.antiguaburgerweb.modelo.OrderEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu implements IGetItems{
     public OrderEntity llenar(String id) {
-        return null;
+        List<String> lleno = new ArrayList<>();
+        OrderEntity menu = new OrderEntity();
+        int precio=0;
+        switch (id) {
+            case "1":
+                lleno.add(id);
+                lleno.add("Huevos");
+                lleno.add("Frijoles");
+                lleno.add("Plátanos");
+                lleno.add("Pan");
+                lleno.add("Bebida");
+                precio = 25;
+                break;
+            case "2":
+                lleno.add(id);
+                lleno.add("Cereal");
+                lleno.add("Fruta");
+                lleno.add("Bebida   ");
+                precio = 18;
+                break;
+            case "3":
+                lleno.add(id);
+                lleno.add("Avena");
+                lleno.add("Fruta");
+                lleno.add("Bebida");
+                precio = 20;
+                break;
+            case "4":
+                lleno.add(id);
+                lleno.add("Hamburguesa");
+                lleno.add("Papas");
+                lleno.add("Bebida");
+                lleno.add("Postre");
+                precio = 30;
+                break;
+            case "5":
+                lleno.add(id);
+                lleno.add("Ensalada");
+                lleno.add("Pollo");
+                lleno.add("Sopa");
+                lleno.add("Bebida");
+                precio = 35;
+                break;
+            case "6":
+                lleno.add(id);
+                lleno.add("Huevos");
+                lleno.add("Frijoles");
+                lleno.add("Tortillas");
+                lleno.add("bebida");
+                precio = 28;
+                break;
+
+        }
+        menu.setTotal(precio);
+        menu.setItems(lleno);
+        return menu;
     }
 }
