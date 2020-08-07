544f0167edd1937f8877c939a99acc3c2fd7bfb6
ronald <ottoniel.21@outlook.es>
2020-08-06 21:47:19 -0600

    Metodos GET y POST añadidos

--- src/gt/com/antiguaburger/antiguaburgerweb/controller/MenuController.java
+++ src/gt/com/antiguaburger/antiguaburgerweb/controller/MenuController.java
@@ -1,13 +1,31 @@
 package gt.com.antiguaburger.antiguaburgerweb.controller;
 
+import gt.com.antiguaburger.antiguaburgerweb.modelo.OrderEntity;
+import gt.com.antiguaburger.antiguaburgerweb.modelo.OrderWebEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class MenuController {
 
-    public String menuForm(Model model) {
-       return null;
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("menu", new OrderWebEntity());
+        return "greeting";
     }
 
-    public String menuSubmit(OrderWebEntity orderw, Model model) {
-        return null;
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute OrderWebEntity orderw, Model model) {
+        OrderEntity order = new OrderEntity();
+        ComboBuilder build = new ComboBuilder();
+        order = build.buildc(orderw);
+        model.addAttribute("menu", order.getItems());
+        model.addAttribute("extras", order.getData());
+        model.addAttribute("total", order.getTotal());
+        return "result";
     }
 
 }
