382e5ec84e6b689de4253e11a71aea8bee38d523
ronald <ottoniel.21@outlook.es>
2020-08-09 17:18:25 -0600

    Creacion del login en el backend

--- src/gt/com/antiguaburger/antiguaburgerweb/controller/ComboBuilder.java
+++ src/gt/com/antiguaburger/antiguaburgerweb/controller/ComboBuilder.java
@@ -3,12 +3,19 @@ package gt.com.antiguaburger.antiguaburgerweb.controller;
 
 import gt.com.antiguaburger.antiguaburgerweb.modelo.OrderEntity;
 import gt.com.antiguaburger.antiguaburgerweb.modelo.OrderWebEntity;
+import gt.com.antiguaburger.antiguaburgerweb.print.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ComboBuilder {
     public OrderEntity buildc (OrderWebEntity orderw){
+        IPrinter txt = new PrintTXT();
+        IPrinter json = new PrintJSON();
+        IPrinter html = new PrintHTML();
+        IPrinter csv = new PrintCSV();
+        IPrinter xml = new PrintXML();
         OrderEntity buildCombo = new OrderEntity();
         OrderDecorator deco = new OrderDecorator();
         OrderEntity items,extras = new OrderEntity();
@@ -29,16 +36,24 @@ public class ComboBuilder {
         for(int a =0; a<orderw.getIdExtra().size();a++) {
             extras = ext.llenar(orderw.getIdExtra().get(a));
             listextra.add(extras.getExtras());
-           // cantidad= Integer.parseInt(orderw.getCantextra().get(a));
+           //cantidad= Integer.parseInt(orderw.getCantextra().get(a));
             totalextras = totalextras + (extras.getTotal()*cantidad);
         }
         items = menu.llenar(orderw.getIdmenu());
 
         listmenu = items.getItems();
+        Date objDate = new Date();
+        buildCombo.setTime(objDate.toString());
+        buildCombo.setCustomer(orderw.getUser());
+        buildCombo.setTaxid(orderw.getNit());
         buildCombo.setData(listextra);
         buildCombo.setTotal(items.getTotal()+totalextras);
         buildCombo.setItems(deco.getDecoratoralgo(listmenu,options));
-
+        txt.print(buildCombo);
+        json.print(buildCombo);
+        html.print(buildCombo);
+        csv.print(buildCombo);
+        xml.print(buildCombo);
         return buildCombo;
     }
 }
--- src/gt/com/antiguaburger/antiguaburgerweb/controller/MenuController.java
+++ src/gt/com/antiguaburger/antiguaburgerweb/controller/MenuController.java
@@ -11,22 +11,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MenuController {
 
+
     @GetMapping("/login")
-    public String loginForm(Model model) {
+    public String indexForm(Model model) {
+        model.addAttribute("login", new OrderWebEntity());
         return "login";
     }
 
-    @GetMapping("/greeting")
+    @PostMapping("/login")
+    public String indexSubmit(@ModelAttribute OrderWebEntity orderw, Model model) {
+        model.addAttribute("index", orderw.getCashier());
+        model.addAttribute("menu",new OrderWebEntity());
+        return "greeting";
+    }
+
+
+    /*@GetMapping("/greeting")
     public String greetingForm(Model model) {
         model.addAttribute("menu", new OrderWebEntity());
         return "greeting";
-    }
+    }*/
 
     @PostMapping("/greeting")
     public String greetingSubmit(@ModelAttribute OrderWebEntity orderw, Model model) {
         OrderEntity order = new OrderEntity();
         ComboBuilder build = new ComboBuilder();
         order = build.buildc(orderw);
+        model.addAttribute("header", order);
         model.addAttribute("menu", order.getItems());
         model.addAttribute("extras", order.getData());
         model.addAttribute("total", order.getTotal());
